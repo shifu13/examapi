4790c1aa8bf2d0b2b964465051df52cbee975bd7
shifu13 <simon.darwin@live.dk>
2020-06-18 12:48:28 +0200

    Testing static data

--- examapi/Controllers/ProductControllerTest.cs
+++ examapi/Controllers/ProductControllerTest.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using examapi.Model;
+using examapi.Data;
 
 namespace examapi.Controllers
 {
-    [Route("api/commands")]
+    [Route("api/producttest")]
     [ApiController]
     public class ProductControllerTest : ControllerBase
     {
+        private readonly MockProductRepo _repository = new MockProductRepo();
+        //Get api/commands
         [HttpGet]
-        public ActionResult <IEnumerable<Product>> GetAllProducts() 
+        public ActionResult<IEnumerable<Product>> GetAllProducts()
         {
+            var productItems = _repository.GetAppProduct();
+            return Ok(productItems);
+        }
 
+        //Get api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProductById(int id) //The id comes from the request from the UI.
+        {
+            var productItem = _repository.GetProductById(id);
+            return Ok(productItem);
         }
     }
 }
